6c5d09f83f5677e0505e29f2e9f068bdf9b3d804
0x00whitejsx <0x00whitejsx@gmail.com>
2024-01-08 18:53:02 +0100

    finish work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const summarizeText = require('./summerize');
 // Parses JSON bodies (as sent by API clients)
 app.use(express.json());
 
@@ -13,11 +13,22 @@ app.use(express.static('public'));
 app.post('/summarize', (req, res) => {
 
    // TODO: handle POST /summarize request
+   const text = req.body.text_to_summarize;
+
+ // call your summarizeText function, passing in the text from the request
+
+  summarizeText(text) 
+    .then(response => {
+       res.send(response); // Send the summary text as a response to the client
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
 
 
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}`);
 });
